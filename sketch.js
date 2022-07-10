6bb3577ef8100e49756bb08789142deccbf54e87
XavierLoera <xavier.loera2@gmail.com>
2022-07-09 21:10:58 -0700

    starfildwarp visualization in processing

---
+++ sketch.js
@@ -0,0 +1,10 @@
+var h = window.innerHeight; //gets window hight
+var w = window.innerWidth; //gets window width
+
+function setup() {
+  createCanvas(w, h); //create a full size canvas
+}
+
+function draw() {
+  background('black'); //change background color to black
+}
